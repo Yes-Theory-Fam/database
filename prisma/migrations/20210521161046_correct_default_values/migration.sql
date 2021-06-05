41b52afd255f4c9894b97ad8deae5c2c58553b27
geisterfurz007 <michelvonv@me.com>
2021-06-05 13:50:51 +0200

    fix: remove endless migration

--- prisma/migrations/20210521161046_correct_default_values/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "deadchat_question" ALTER COLUMN "last_used" SET DEFAULT 'now'::timestamp without time zone;
-
--- AlterTable
-ALTER TABLE "topic" ALTER COLUMN "created" SET DEFAULT 'now'::timestamp without time zone;
-
--- AlterTable
-ALTER TABLE "user_group" ALTER COLUMN "last_used" SET DEFAULT ('now'::timestamp without time zone - '01:00:00'::interval);
